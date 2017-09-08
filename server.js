f8d5a53a46da451b4c85f8bf72404ee289ec994e
sandi1234 <sandi1234@users.noreply.github.com>
2017-09-08 10:12:28 +0200

    Update server.js
    Put the access control in the function

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // http
 var http = require('http');
-res.setHeader('Access-Control-Allow-Origin', '*');
+
 var portNumber = 8000;
 
 var server = http.createServer(function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.writeHead(200, {'Content-type' : 'text/json'});
     res.write('{"title": "Hello JSON web API", "text" : "Text fra min API"}');
     res.end();
